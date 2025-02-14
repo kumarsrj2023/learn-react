aa5c34f73f9fb9c84044fa2a75d573e59f95b561
Suraj kumar <kumarsrj2023@gmail.com>
2025-02-14 16:28:17 +0530

    add users component

---
+++ src/Users.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Users() {
+  return (
+    <>
+    <div className='user-list'>
+        <h1>User List</h1>
+        {/* List of users */}
+        <ul>
+          <li>User 1</li>
+          <li>User 2</li>
+          <li>User 3</li>
+        </ul>
+    </div>
+    </>
+  )
+}
+
+export default Users
