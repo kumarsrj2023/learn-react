a11c49925b134fe856efea4f266d5e5c95a45947
Suraj kumar <kumarsrj2023@gmail.com>
2025-02-14 17:01:53 +0530

    install tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Users from './Users'
+import Header from './layouts/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Header />
     <Users />
     </>
   )
---
+++ src/layouts/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+   <>
+   <h1 className="text-5xl font-bold underline ">
+      This is header
+    </h1>
+   </>
+  )
+}
+
+export default Header
